e195a794d9f47f618be2c2a4aec1c08f9a64db2a
Krittik <krittikkakati@gmail.com>
2023-07-11 21:47:43 +0530

    edited

--- poc2/poc2gui.py
+++ poc2/poc2gui.py
@@ -1,8 +1,28 @@
-from PIL import Image, ImageTk
-import tkinter as tk
+from tkinter import *
+from PIL import ImageTk, Image
 import os
 
-directory_path = "python/handsignPOC/poc2"
+root = Tk()
+root.geometry("600x600")
 
-window = tk.Tk()
+letters = ['A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
+my_dict = {}
+
+for letter in letters:
+    my_dict[letter] = letter + ".jpg"
+
+def show_image():
+    letter = letter_input.get().upper()
+    img = ImageTk.PhotoImage(Image.open(os.path.join("python/TechVariable/poc2", my_dict[letter])))
+    panel = Label(root, image=img)
+    panel.image = img
+    panel.pack()
+
+letter_input = Entry(root)
+letter_input.pack()
+
+button = Button(root, text="Show Image", command=show_image)
+button.pack()
+
+root.mainloop()
