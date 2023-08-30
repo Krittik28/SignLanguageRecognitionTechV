3d9b3261d394ba71eb5d5658ab096d9c08d59112
Krittik28 <94062268+Krittik28@users.noreply.github.com>
2023-08-30 15:37:48 +0530

    Update splitting.py

--- splitting.py
+++ splitting.py
@@ -2,10 +2,8 @@ import os
 import random
 from pathlib import Path
 
-# Define the source directory containing the letter folders (A, B, C, ..., Z)
 source_dir = 'python/TechVariable/data'
 
-# Define the output directory for the training and testing sets
 output_dir = 'python/TechVariable/main_dataset'
 training_dir = Path(output_dir, 'training')
 testing_dir = Path(output_dir, 'testing')
@@ -13,7 +11,6 @@ testing_dir = Path(output_dir, 'testing')
 if not Path(output_dir).exists():
     Path(output_dir).mkdir(parents=True)
 
-# Function to preprocess images in a folder
 def preprocess_images(letter_dir, output_letter_dir, split_ratio=0.8):
     images = os.listdir(letter_dir)
     random.shuffle(images)
@@ -35,7 +32,6 @@ def preprocess_images(letter_dir, output_letter_dir, split_ratio=0.8):
         output_path.parent.mkdir(parents=True, exist_ok=True)
         output_path.write_bytes(image_path.read_bytes())
 
-# Loop through all the letter directories (A, B, C, ..., Z)
 for letter in range(ord('A'), ord('Z')+1):
     letter_folder = chr(letter)
     letter_dir = Path(source_dir, letter_folder)
